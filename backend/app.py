32c8da46523827376c7463ce1ebd3efbf90c7dff
kitodev <tkornel19@gmail.com>
2020-12-25 12:31:09 +0100

    GetText class init

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,35 @@
-from flask import Flask, render_template
-from flask import jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, request
+import os, pytesseract
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from PIL import Image
 
-app = Flask(__name__,
-            static_folder='../frontend/dist/static',
-            template_folder='../frontend/dist')
+project_dir = os.path.dirname(os.path.abspath(__file__))
 
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app = Flask(__name__, static_url_path='',static_folder="static", template_folder="templates")
 
-@app.route('/api/v1/message')
-def message():
-    return jsonify("This is a first message")
+img = UploadSet('photos', IMAGES)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def render_vue(path):
-    return render_template("index.html")
+app.config['UPLOAD_FOLDER'] = 'images'
 
+class GetText(object):
+    def __init__(self, file):
+        self.file = pytesseract.image_to_string(Image.open(project_dir + '/images/' + file))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'photo' not in request.files:
+            return 'There is no image on form'
+        img_name = request.form['image-name'] + '.jpg'
+        img = request.files['photo']
+        texta = request.form['text']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+        img.save(path)
+
+        text = GetText(img_name)
+        
+        return text.file
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(debug=True)
